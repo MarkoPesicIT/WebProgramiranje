fe99ef9c5fec77b0af60a347799e54283a9c9d6a
KrticaTheMadLad <100162583+KrticaTheMadLad@users.noreply.github.com>
2023-11-24 21:40:39 +0100

    #kjasghdjhgkasd

--- scripts.js
+++ scripts.js
@@ -5,11 +5,36 @@ function openPopup(title, content) {
   }
   
   function closePopup() {
-      document.getElementById('popup').style.display = 'none';
+    const popup = document.getElementById('popup');
+    popup.classList.add('closing');
+    setTimeout(() => {
+      popup.style.display = 'none';
+      popup.classList.remove('closing');
+    }, 500);
   }
+
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      closePopup();
+    }
+  });
+
   function openPopup(title, shortDescription, fullText, imageUrl) {
     document.getElementById('popup-title').innerText = title;
     document.getElementById('popup-content').innerHTML = `<p>${shortDescription}</p><p>${fullText}</p>`;
     document.getElementById('popup-image').src = imageUrl;
     document.getElementById('popup').style.display = 'block';
 }
+function viewmore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      moreText.style.display = "inline";
+    }
+  }
